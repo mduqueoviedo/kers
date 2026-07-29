0b05ef0a6020034de67b8c367142bcc14ae3df7d
Marcos Duque <mduqueoviedo@users.noreply.github.com>
2026-07-29 10:01:39 +0200

    Self-host Instrument Sans (#6)

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import {
     defineConfig
 } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
+import { fontsource } from 'laravel-vite-plugin/fonts';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
@@ -14,7 +14,7 @@ export default defineConfig({
             ],
             refresh: true,
             fonts: [
-                bunny('Instrument Sans', {
+                fontsource('Instrument Sans', {
                     weights: [400, 500, 600],
                 }),
             ],
